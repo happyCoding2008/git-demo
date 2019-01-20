02c3bb16b28388256cbb23f9c6781e46fd79d356
hgz <2247974482@qq.com>
2019-01-20 15:34:55 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/gitdemo/GitDemoApplication.java
@@ -0,0 +1,14 @@
+package com.qianfeng.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/qianfeng/gitdemo/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.qianfeng.gitdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author HuangGuiZhao
+ * @Date 2019/1/20
+ */
+@Controller
+@RequestMapping("web")
+public class WebController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        System.out.println("hello");
+        return "hello,git";
+    }
+}
